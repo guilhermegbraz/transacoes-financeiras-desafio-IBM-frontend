3a88eab4c8df430c7c06c4eff1a7a3e98c7b529f
Guilherme <guilherme.g.braz@outlook.com>
2024-05-30 17:11:09 -0300

    componente cabecalho

---
+++ src/app/shared/cabecalho/cabecalho.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {NgIf, NgOptimizedImage} from "@angular/common";
+import {Router} from "@angular/router";
+
+
+@Component({
+  selector: 'app-cabecalho',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, NgOptimizedImage, NgIf],
+  templateUrl: './cabecalho.component.html',
+  styleUrl: './cabecalho.component.scss'
+})
+export class CabecalhoComponent {
+  @Input() exibirBotaoHome: boolean = false
+
+  constructor(private router: Router) {
+  }
+
+  navegarHome(): void {
+    this.router.navigate([""]);
+  }
+}
