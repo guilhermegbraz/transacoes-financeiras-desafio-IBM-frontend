b80fb6d67ba194e031c11db0894540894734d81b
Guilherme <guilherme.g.braz@outlook.com>
2024-05-31 00:20:28 -0300

    adicao botões dinamicos na tabela

--- src/app/shared/tabela-paginavel/tabela-paginavel.component.ts
+++ src/app/shared/tabela-paginavel/tabela-paginavel.component.ts
@@ -1,7 +1,19 @@
-import {Component, Input, OnChanges, SimpleChanges, ViewChild, AfterViewInit} from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+  AfterViewInit,
+  Output,
+  EventEmitter
+} from '@angular/core';
+import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatTable, MatTableModule} from '@angular/material/table';
-import {MatPaginator, MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 export interface DadosTabela<T> {
   dados: T[];
@@ -11,36 +23,33 @@ export interface CabecalhoTabela {
   nomeAtributo: string,
   labelAtributo: string;
 }
+
+export interface BotaoAcao {
+  acao: string,
+  icone: string,
+  descricao: string;
+}
+export interface EntidadeAcao<T> {
+  entidade: T,
+  acao: string
+
+}
 @Component({
   selector: 'app-tabela-paginavel',
   standalone: true,
-  imports: [MatTableModule, CommonModule, MatPaginatorModule],
+  imports: [MatTableModule, CommonModule, MatButtonModule, MatTooltipModule, MatIconModule],
   templateUrl: './tabela-paginavel.component.html',
   styleUrl: './tabela-paginavel.component.scss'
 })
 
 export class TabelaPaginavelComponent<T> implements AfterViewInit, OnChanges{
   @Input() colunas: CabecalhoTabela[] = [];
-  colunasExibidas: string[] = []
   @Input() dataSource!: DadosTabela<T>;
-  @ViewChild(MatTable) table!: MatTable<any>;
-  length = 50;
-  pageSize = 10;
-  pageIndex = 0;
-  pageSizeOptions = [5, 10, 25];
-
-  hidePageSize = false;
-  showPageSizeOptions = true;
-  showFirstLastButtons = true;
-  disabled = false;
-
-
-
-  handlePageEvent(e: PageEvent) {
-    this.length = e.length;
-    this.pageSize = e.pageSize;
-    this.pageIndex = e.pageIndex;
-  }
+  @Input() exibirBotoesAcao: boolean = false;
+  @Input() botoesAcao: BotaoAcao[] = [];
+  @ViewChild(MatTable) table!: MatTable<T>;
+  @Output() clickBotaoAcao: EventEmitter<EntidadeAcao<T>> = new EventEmitter<EntidadeAcao<T>>()
+  colunasExibidas: string[] = []
   ngAfterViewInit(): void {
     this.renderTable()
   }
@@ -51,10 +60,13 @@ export class TabelaPaginavelComponent<T> implements AfterViewInit, OnChanges{
   public renderTable(): void {
     if(this.table) this.table.renderRows();
     this.colunasExibidas = this.colunas.map(c => c.nomeAtributo)
+    if(this.exibirBotoesAcao) this.colunasExibidas.push("Ação");
+
   }
 
-
-
+  public emitirClickBotao(registro: T, acao: string) {
+    this.clickBotaoAcao.emit({entidade: registro, acao: acao})
+  }
 
 
 }
