58eed6510b6e735b316fef10ad78f457822cae62
Guilherme <guilherme.g.braz@outlook.com>
2024-05-31 11:43:01 -0300

    home buscando dados na API e navegação concluida

--- src/app/page/home-cliente/home-cliente.component.ts
+++ src/app/page/home-cliente/home-cliente.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CabecalhoComponent} from "../../shared/cabecalho/cabecalho.component";
 import {RodapeComponent} from "../../shared/rodape/rodape.component";
 import {ContainerComponent} from "../../shared/container/container.component";
@@ -8,10 +8,14 @@ import {PageEvent} from '@angular/material/paginator';
 import {
   BotaoAcao,
   CabecalhoTabela,
-  DadosTabela,
+  DadosTabela, EntidadeAcao,
   TabelaPaginavelComponent
 } from "../../shared/tabela-paginavel/tabela-paginavel.component";
 import {ClienteConta} from "../../domain/model/cliente-conta.model";
+import {ClienteContaService} from "../../domain/services/cliente-conta.service";
+import {ClienteApiToClienteContaParser} from "../../domain/parser/clienteApi-to-clienteConta.parser";
+import {ClienteApi, ClietePageable} from "../../domain/response-api/cliente-response";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -28,20 +32,9 @@ import {ClienteConta} from "../../domain/model/cliente-conta.model";
   templateUrl: './home-cliente.component.html',
   styleUrl: './home-cliente.component.scss'
 })
-export class HomeClienteComponent {
-  dadosClietntes:ClienteConta[] = [
-    {idCliente: 5, nome: "Guilherme", email: "g@email.com", numeroConta:"65027810",  idConta: 62, saldo: 845.32, creditoDisponivel: 1255},
-    {idCliente: 5, nome: "Guilherme", email: "g@email.com", numeroConta:"65027810",  idConta: 62, saldo: 845.32, creditoDisponivel: 1255},
-    {idCliente: 5, nome: "Guilherme", email: "g@email.com", numeroConta:"65027810",  idConta: 62, saldo: 845.32, creditoDisponivel: 1255},
-    {idCliente: 5, nome: "Guilherme", email: "g@email.com", numeroConta:"65027810",  idConta: 62, saldo: 845.32, creditoDisponivel: 1255},
-    {idCliente: 5, nome: "Guilherme", email: "g@email.com", numeroConta:"65027810",  idConta: 62, saldo: 845.32, creditoDisponivel: 1255},
-    {idCliente: 5, nome: "Guilherme", email: "g@email.com", numeroConta:"65027810",  idConta: 62, saldo: 845.32, creditoDisponivel: 1255},
-    {idCliente: 5, nome: "Guilherme", email: "g@email.com", numeroConta:"65027810",  idConta: 62, saldo: 845.32, creditoDisponivel: 1255},
-    {idCliente: 5, nome: "Guilherme", email: "g@email.com", numeroConta:"65027810",  idConta: 62, saldo: 845.32, creditoDisponivel: 1255},
-    {idCliente: 5, nome: "Guilherme", email: "g@email.com", numeroConta:"65027810",  idConta: 62, saldo: 845.32, creditoDisponivel: 1255}
-  ]
-  dadosTabela : DadosTabela<ClienteConta>= {dados: this.dadosClietntes}
-  colunasExibicao: CabecalhoTabela[] = [
+export class HomeClienteComponent implements OnInit{
+
+  COLUNAS_EXIBIDAS: CabecalhoTabela[] = [
     {nomeAtributo: "idCliente", labelAtributo: "Identificador"},
     {nomeAtributo: "nome", labelAtributo: "Nome cliente"},
     {nomeAtributo: "email", labelAtributo: "Email cliente"},
@@ -50,17 +43,45 @@ export class HomeClienteComponent {
     {nomeAtributo: "creditoDisponivel", labelAtributo: "Credito disponível"},
   ]
 
+  dadosCliente: ClienteConta[] = [];
+  dadosTabela: DadosTabela<ClienteConta>;
   botoes: BotaoAcao[];
-  constructor() {
-    this.botoes = [{acao: "detalharTransacao", icone: "open_in_new", descricao: "Detalhar as transações financeiras da conta"}]
+  quantidadeRegistros: number = 0;
+  tamanhoPagina: number = 10;
+  numeroPagina: number = 0;
+  opcoesTamanhoPagina: number[] = [5, 10, 15, 25, 50]
+
+  ACAO_DETALHAR_TRANSACAO = "detalharTransacao"
+
+  constructor(private router: Router, private clienteContaService: ClienteContaService, private clienteApiParser: ClienteApiToClienteContaParser) {
+    this.botoes = [{acao: this.ACAO_DETALHAR_TRANSACAO, icone: "open_in_new", descricao: "Detalhar as transações financeiras da conta"}]
+    this.dadosTabela = {dados: []}
   }
 
+  ngOnInit(): void {
+    this.carregarRegistros();
+  }
+  private carregarRegistros(): void {
+    this.clienteContaService.listar(this.tamanhoPagina,this.numeroPagina).subscribe((response: ClietePageable) => {
+      this.quantidadeRegistros = response.totalElements
+      this.dadosCliente = response.content.map((clienteApi: ClienteApi) => this.clienteApiParser.parse(clienteApi));
+      this.dadosTabela.dados = this.dadosCliente;
+    })
+  }
   public adicionarCliente(): void {
-   this.dadosClietntes.push({idCliente: 97, nome: "Joao", email: "joao@email.com", numeroConta:"32123456",  idConta: 62, saldo: 125.32, creditoDisponivel: 378})
-    this.dadosTabela = {dados: this.dadosClietntes}
+   this.dadosCliente.push({idCliente: 97, nome: "Joao", email: "joao@email.com", numeroConta:"32123456",  idConta: 62, saldo: 125.32, creditoDisponivel: 378})
+    this.dadosTabela = {dados: this.dadosCliente}
   }
 
- public mudarPagina(evento: PageEvent): void {
-    console.log("Alterando pagina")
- }
+   public mudarPagina(evento: PageEvent): void {
+      this.numeroPagina = evento.pageIndex;
+      this.tamanhoPagina = evento.pageSize
+     this.carregarRegistros()
+   }
+
+   handleBotaoAcao(evento:  EntidadeAcao<ClienteConta>): void {
+     if(evento.acao == this.ACAO_DETALHAR_TRANSACAO) this.router.navigate([`conta/${evento.entidade.idConta}`])
+   }
+
+
 }
