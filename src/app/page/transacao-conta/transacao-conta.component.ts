fc8f2b32691806bbfbaa3e011348e15e3b69e64e
Guilherme <guilherme.g.braz@outlook.com>
2024-05-31 13:47:51 -0300

    pagina transacao com paginação funcionando + exibição saldo cliente

--- src/app/page/transacao-conta/transacao-conta.component.ts
+++ src/app/page/transacao-conta/transacao-conta.component.ts
@@ -11,6 +11,14 @@ import {
   TabelaPaginavelComponent
 } from "../../shared/tabela-paginavel/tabela-paginavel.component";
 import {ActivatedRoute} from "@angular/router";
+import {TransacaoApiToTransacaoParser} from "../../domain/parser/transacaoApi-to-transacao.parser";
+import {TransacaoFinanceiraService} from "../../domain/services/transacao-financeira.service";
+import {TransacaoApi} from "../../domain/response-api/transacao-response";
+import {TransacaoFinanceira} from "../../domain/model/transacao.model";
+import {PageEvent} from "@angular/material/paginator";
+import {ClienteContaService} from "../../domain/services/cliente-conta.service";
+import {ClienteApi} from "../../domain/response-api/cliente-response";
+import {HttpResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-transacao-conta',
@@ -27,21 +35,70 @@ import {ActivatedRoute} from "@angular/router";
   styleUrl: './transacao-conta.component.scss'
 })
 export class TransacaoContaComponent implements OnInit{
-  private idConta: string| null = null;
-  idContaRecebido: boolean = false
-  dadosTabela!: DadosTabela<any>;
-  colunasExibicao!:  CabecalhoTabela[];
+  colunasExibicao : CabecalhoTabela[] = [
+    {nomeAtributo: "id", labelAtributo: "Identificador transação"},
+    {nomeAtributo: "tipoTransacao", labelAtributo: "Tipo da transação"},
+    {nomeAtributo: "valor", labelAtributo: "Valor monetario"},
+    {nomeAtributo: "data", labelAtributo: "Data transação"},
+    {nomeAtributo: "descricao", labelAtributo: "Descrição"},
+  ];
+
+  subtitulo: string;
+  botaoAdicionarRegistroApto: boolean;
+  private idConta: number;
+  idContaRecebido: boolean;
+  dadosTabela!: DadosTabela< TransacaoFinanceira>;
+  quantidadeRegistros: number = 0;
+  tamanhoPagina: number = 10;
+  numeroPagina: number = 0;
+  opcoesTamanhoPagina: number[] = [5, 10, 15, 25, 50]
+
   botoes!: BotaoAcao[];
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private transacaoApiParser: TransacaoApiToTransacaoParser,
+              private transacaoService: TransacaoFinanceiraService,
+              private clienteService: ClienteContaService) {
+    this.idConta = 0;
+    this.idContaRecebido = false;
+    this.subtitulo = "Conta inexistente, volte para tela inicial"
+    this.botaoAdicionarRegistroApto = false;
   }
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
-      this.idConta = params.get('id');
-      if (this.idConta != null) this.idContaRecebido = true;
+      let idConta = params.get('id');
+      if (idConta != null) {
+        this.idConta = Number(idConta);
+        this.idContaRecebido = true;
+        this.carregarRegistros();
+        this.carregarInformacaoConta();
+      }
     });
+  }
+
+  private carregarRegistros(): void {
+    this.transacaoService.listarTransacaoByIdConta(
+      this.idConta! as number, this.tamanhoPagina, this.numeroPagina
+    ).subscribe((response) => {
+      this.dadosTabela = {dados: response.content.map((transacaoApi: TransacaoApi) => this.transacaoApiParser.parse(transacaoApi))}
+      this.quantidadeRegistros = response.totalElements
+    })
+  }
+
+  private carregarInformacaoConta():void {
+    this.clienteService.detalharCliente(this.idConta).subscribe((response: HttpResponse<ClienteApi>) => {
+      if(response['status']== 200 ) {
+        this.botaoAdicionarRegistroApto = true
+        let cliente = response.body!
+        this.subtitulo = `Cliente: ${cliente.nome} | Saldo: ${cliente.contaBancaria.saldo} | Crédito disponível: ${cliente.contaBancaria.creditoDisponivel}`;
+      }
+    })
   }
 
   adicionarTransacao(): void {}
 
-  mudarPagina(event: any): void {}
+  public mudarPagina(evento: PageEvent): void {
+    this.numeroPagina = evento.pageIndex;
+    this.tamanhoPagina = evento.pageSize
+    this.carregarRegistros()
+  }
+
 }
