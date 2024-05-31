c5ef34287781932b128797d4f210b4c7df652ddc
Guilherme <guilherme.g.braz@outlook.com>
2024-05-31 00:21:02 -0300

    cliente model

---
+++ src/app/domain/model/cliente-conta.model.ts
@@ -0,0 +1,9 @@
+export interface ClienteConta {
+  idCliente: number,
+  nome: string,
+  email: string,
+  numeroConta: string,
+  idConta: number,
+  saldo: number,
+  creditoDisponivel: number
+}
