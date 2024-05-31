3d8bc7ac5f9b65d144e65511f7886f4b5b98218e
Guilherme <guilherme.g.braz@outlook.com>
2024-05-31 18:08:12 -0300

    método para cadastrar cliente

--- src/app/domain/services/cliente-conta.service.ts
+++ src/app/domain/services/cliente-conta.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpResponse} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {environment} from "../../../environments/environment";
-import {ClienteApi, ClietePageable} from "../response-api/cliente-response";
+import {ClienteApi, ClietePageable, ContaBancariaApi} from "../response-api/cliente-response";
+import {CadastroClienteDto} from "../response-api/cadastro-cliente.dto";
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,15 @@ export class ClienteContaService {
   private apiUrl: string = environment.apiUrl;
   constructor(private httpClient: HttpClient) { }
 
-  listar(tamanhoPagina: number, numeroPagina: number): Observable<ClietePageable> {
-    return this.httpClient.get<ClietePageable>(`${this.apiUrl}/cliente?page=${numeroPagina}&size=${tamanhoPagina}`)
+  listar(tamanhoPagina: number, numeroPagina: number): Observable<HttpResponse<ClietePageable>> {
+    return this.httpClient.get<ClietePageable>(`${this.apiUrl}/cliente?page=${numeroPagina}&size=${tamanhoPagina}`, { observe: 'response' })
   }
 
-  detalharCliente(idCliente: number): Observable<HttpResponse<ClienteApi>> {
-    return this.httpClient.get<ClienteApi>(`${this.apiUrl}/cliente/${idCliente}`, { observe: 'response' })
+  detalharClienteContaByIdConta(idCliente: number): Observable<HttpResponse<ContaBancariaApi>> {
+    return this.httpClient.get<ContaBancariaApi>(`${this.apiUrl}/conta/${idCliente}`, { observe: 'response' })
+  }
+
+  public cadastrarCliente(cliente: CadastroClienteDto): Observable<HttpResponse<ClienteApi>> {
+    return this.httpClient.post<ClienteApi>(`${this.apiUrl}/cliente`, cliente, { observe: 'response' })
   }
 }
